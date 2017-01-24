be777faba5102c8cb860d9106f4254700fa737fc
daniel.freitas <danilimah18@gmail.com>
2017-01-24 15:38:51 -0200

    x = 5

--- app/src/main/java/com/example/danielfreitas/eupegava/MainActivity.java
+++ app/src/main/java/com/example/danielfreitas/eupegava/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int x = 0;
+        int x = 5;
     }
 }
