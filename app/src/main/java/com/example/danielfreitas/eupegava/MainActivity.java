c70d932a1a96fd527599ddd880c97f8ee48922eb
Marcel <reyneres@gmail.com>
2017-01-24 16:12:05 -0200

    10

--- app/src/main/java/com/example/danielfreitas/eupegava/MainActivity.java
+++ app/src/main/java/com/example/danielfreitas/eupegava/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int x = 5;
+        int x = 10;
     }
 }
