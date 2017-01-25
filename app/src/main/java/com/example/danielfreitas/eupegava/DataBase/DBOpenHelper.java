6fa0b4d9e6772c9e07b0f641b33fc85998ea970a
daniel.freitas <danilimah18@gmail.com>
2017-01-25 17:43:01 -0200

    db created

---
+++ app/src/main/java/com/example/danielfreitas/eupegava/DataBase/DBOpenHelper.java
@@ -0,0 +1,30 @@
+package com.example.danielfreitas.eupegava.DataBase;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by daniel.freitas on 1/25/17.
+ */
+
+public class DBOpenHelper extends SQLiteOpenHelper {
+    //classe que cria tabelas e banco de dados
+
+    private static final int DATABASE_VERSION = 2;
+    private static final String DATABASE_NAME = "DataBase";
+
+    public DBOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
